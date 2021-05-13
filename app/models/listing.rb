96faba8744741505058a466a104fd47d7e8f5980
Nat <ntaktach@gmail.com>
2021-05-13 11:45:41 -0400

    validate

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,11 @@
 class Listing < ApplicationRecord
     # belongs_to :user
+
+    validates :imgsrc, presence: true
+    validates :brand, presence: true
+    validates :year, presence: true
+    validates :size, presence: true
+    validates :description, presence: true
+    validates :title, presence: true 
+    
 end
